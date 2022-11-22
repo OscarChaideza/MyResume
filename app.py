83fba4f985445a0f3d2e4a7b25b595c849bf606a
OscarChaideza <oscar_chaideza@hotmail.com>
2022-11-22 09:07:32 -0600

    changes app.py templates

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-#Hello
+# Import library and packages 
+from flask import Flask, render_template
+
+# Create our application
+app = Flask(__name__)
+
+# Create our routes
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template ('aboutpage.html')
+
+
+#Start our application
+if __name__ == "__main__":
+    app.run(debug=True)  
+    
